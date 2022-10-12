6f636b47074a71bffe57329f11e6c23bcdd0cea6
naomina121 <naomina@me.com>
2022-10-13 05:12:38 +0900

    feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,62 +108,56 @@ module.exports = {
       `gatsby-remark-prismjs`,
       `gatsby-remark-copy-linked-files`,
       `gatsby-remark-smartypants`,
+
       {
-        resolve: `gatsby-plugin-feed`,
-        options: {
-          query: `
-            {
-              site {
-                siteMetadata {
-                  title
-                  description
-                  siteUrl
-                  site_url: siteUrl
-                }
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
               }
             }
-          `,
-          feeds: [
-            {
-              serialize: ({ query: { site, allMarkdownRemark } }) => {
-                return allMarkdownRemark.nodes.map(node => {
-                  return Object.assign({}, node.frontmatter, {
-                    description: node.excerpt,
-                    date: node.frontmatter.date,
-                    url: site.siteMetadata.siteUrl + node.fields.slug,
-                    guid: site.siteMetadata.siteUrl + node.fields.slug,
-                    custom_elements: [{ "content:encoded": node.html }],
-                  })
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + node.fields.slug,
+                  custom_elements: [{ "content:encoded": node.html }],
                 })
-              },
-              query: `
-                {
-                  allMarkdownRemark(
-                    sort: { order: DESC, fields: [frontmatter___date] },
-                  ) edges {
-                    nodes {
-                      excerpt
-                      html
-                      fields {
-                        slug
-                      }
-                      frontmatter {
-                        title
-                        date(formatString: "YYYY.MM.DD")
-                        hero
-                        cate
-                        tags
-                        modifieddate
-                        description
-                      }
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  nodes {
+                    excerpt
+                    html
+                    fields {
+                      slug
+                    }
+                    frontmatter {
+                      title
+                      date
                     }
                   }
                 }
-              `,
-              output: "/rss.xml",
-              title: "ナオのメンタルヘルス",
-              feed_url: "https://naomina.com/feed/rss.xml",
-              site_url: "https://naomina.com/",
+              }
+            `,
+            output: "/rss.xml",
+            title: "ナオのメンタルヘルス",
           },
         ],
       },
