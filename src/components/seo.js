0e5cd60a7aa50e29d85a5e78eeffb8960949edb1
naomina121 <naomina@me.com>
2022-10-13 06:05:19 +0900

    searchconsole

--- src/components/seo.js
+++ src/components/seo.js
@@ -70,6 +70,10 @@ const Seo = ({ description, lang, meta, title }) => {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name:`google-site-verification`,
+          content: `fXFsNigYLZtuUktAMNlINqOOcUs6qjp8Lsu2fyh_xkA`,
+        },
       ].concat(meta)}
     />
   )
